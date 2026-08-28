69fd8a79268617d4a3e4943ee01c028df7a22c38
潘杰 <1351982581@qq.com>
2026-08-28 16:08:48 +0800

    test(e2e): run responsive project on chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   projects: [
     { name: "chromium", use: { ...devices["Desktop Chrome"] } },
-    { name: "mobile", use: { ...devices["iPhone 13"] } },
+    { name: "mobile", use: { ...devices["iPhone 13"], browserName: "chromium" } },
   ],
   webServer: [
     {
